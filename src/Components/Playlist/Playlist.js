992ed2634e1423e4e3ed942ddbd0044f8eb5a1a4
jdelpego <jdelpego2@gmail.com>
2024-09-09 16:13:47 -0700

    search by using enter

--- src/Components/Playlist/Playlist.js
+++ src/Components/Playlist/Playlist.js
@@ -13,7 +13,24 @@ function Playlist() {
     const [search, setSearch] = useState('');
     const [searchMode, setSearchMode] = useState(false);
 
-    const searchHandler = () => {
+    const submitHandler = (event) => {
+        event.preventDefault(); 
+        if (search === ''){
+            alert('Search value must not be blank');
+            }
+            else{
+            setSearchMode(true);
+            getSpotifyAccessToken().then(() => search_request(search).then((data) => 
+                {
+                    console.log(JSON.stringify(data));
+                    setSearchTracks(convertDataToTracksArray(data));
+                }
+            ));
+            }
+    };
+
+    const searchHandler = (event) => {
+        event.preventDefault(); 
         if(searchMode === true){
             setSearchMode(false);
         }
@@ -37,7 +54,10 @@ function Playlist() {
         <div className={styles.playlistBox}>
             <div className={styles.playlist}>
                 <input className={styles.title} type="text" value={name} onChange={(e) => setName(e.target.value)} />
-                <input className={styles.search} type="text" value={searchMode ? search : ''} onChange={(e) => setSearch(e.target.value)} />
+                <form className={styles.form} onSubmit={submitHandler}>
+                    <input className={styles.search} type="text" value={search} onChange={(e) => setSearch(e.target.value)} />
+                </form>
+                
                 <Tracklist tracks={searchMode ? searchTracks : playlistTracks} setPlaylistTracks={setPlaylistTracks} isSearchMode={searchMode} setSearchTracks={setSearchTracks}/>
                 <footer className={styles.footer}>
                     <button className={styles.button} style={{backgroundColor: searchMode ? 'lightyellow': 'lightblue'}} onClick={searchHandler}> {searchMode ? 'Back To Playlist' : 'Search'}</button>
